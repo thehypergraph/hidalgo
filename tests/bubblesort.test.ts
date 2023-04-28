1c96b1b6a2b2629bfb387a3c930aea936537347c
Ronald Jordan <amisword@gmail.com>
2023-04-27 22:16:27 -0700

    Add bubblesort

---
+++ tests/bubblesort.test.ts
@@ -0,0 +1,9 @@
+import { assertEquals } from '../deps.ts'
+import { bubblesort } from '../src/bubblesort.ts'
+
+Deno.test('search a small array', () => {
+  const arr = [5, 8, 2, 3, 4, 9, 10]
+  const res = bubblesort(arr)
+  console.log(res)
+  assertEquals(res, [2, 3, 4, 5, 8, 9, 10])
+})
